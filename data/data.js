0e1ac6f8ca31db7bb5cfa0cb5a09d8fbb95d1fea
Dvent1123 <ventura.daniel36@gmail.com>
2021-04-21 18:41:45 -0700

    added real data for skills

--- data/data.js
+++ data/data.js
@@ -3,21 +3,21 @@ exports.exerciseData = [
         id: 0,
         exercise: 'planche',
         locked: true,
-        diff: 3,
-        desc: ' It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+        diff: 10,
+        desc: 'Popularized in gymnastics the planche is one of the most beautiful and difficult calisthenics moves. The difficulty comes from the disadvantageous position it puts you in with most of your weight on your shoulders. Master this to feel the weight of the world on your shoulders.',
     },
     {
         id: 1,
         exercise: 'frontLever',
-        locked: false,
-        diff: 3,
-        desc: ' It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Why do we use it? It is a long established fact that a reader will be distracted by the readable ',
+        locked: true,
+        diff: 7,
+        desc: "The front lever is a move where your body is completely horizontal, in a straight line, and with the front of your body facing upwards. Although this is challenging to your core it is really a test of your pulling strength. If you can do this you'll have a demon back",
     },
     {
         id: 2,
         exercise: 'pullup',
         locked: true,
-        diff: 2,
-        desc: ' It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more',
+        diff: 3,
+        desc: 'Possibly the king of back exercises. The pullup is a calisthenic athletes go-to for back development. The concept is simple, start at a dead hang, lift yourself up until your chin is above the bar, control the way down. Words can be deceiving. This should be among your first goals.',
     }
 ]
